b040ce6f26105168947267b15998e6a37cf64184
Jonatas de Oliveira Monteiro <jonatasoliveiramont@gmail.com>
2022-07-20 12:45:59 -0300

    feat/create exemples children cards

---
+++ praticando-children-jonatasoliveiraa/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import CenteredCard from "./components/CenteredCard/CenteredCard";
+
+function App() {
+  return (
+    <div className="App">
+      <CenteredCard>
+        <h1>Chidren 1</h1>
+        <h1>Chidren 2</h1>
+        <h1>Chidren 3</h1>
+
+      </CenteredCard>
+    </div>
+  );
+}
+
+export default App;
